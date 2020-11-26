d6d0616d42439b01229172bcee77025ce70bcc08
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-26 15:35:42 -0800

    commit tree 34f0b87d10965241f5303ec2ce6ac8bbf3ac2baf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+require 'pry'
+
+
+class Anagram
+
+    attr_accessor :phrase
+
+    def initialize (phrase)
+        @phrase = phrase 
+    end 
+
+    def match(array)
+        array.keep_if do |element|
+            phrase.split('').sort == element.split('').sort
+        end 
+    end  
+end 
+
+
